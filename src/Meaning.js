ad615adb5236b1150101f19564ca251fdcfef66c
DanniFront <mure.daniela@gmail.com>
2025-06-16 15:10:49 +0200

    Changes in map

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,8 +4,8 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definitions.definition}</p>
-      <p>{props.meaning.definitions.example}</p>
+      <p>{props.meaning.definition}</p>
+      <p>{props.meaning.example}</p>
     </div>
   );
 }
