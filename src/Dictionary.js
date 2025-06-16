17e9edd0a11938e0b88a98e1d966256ede08204f
DanniFront <mure.daniela@gmail.com>
2025-06-16 13:06:03 +0200

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,13 +4,20 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiKey = `748d5734be19te1af0e3a12aa9abaofd`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data);
   }
 
   return (
