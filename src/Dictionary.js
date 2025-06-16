97963778f0bc1903cd630ed2e532f84b97da2d15
DanniFront <mure.daniela@gmail.com>
2025-06-16 14:52:11 +0200

    Added new component Meaning and mapping

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function search(event) {
     event.preventDefault();
-    let apiKey = `748d5734be19te1af0e3a12aa9abaofd`;
+    let apiKey = "748d5734be19te1af0e3a12aa9abaofd";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -18,6 +20,7 @@ export default function Dictionary() {
 
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data);
   }
 
   return (
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
