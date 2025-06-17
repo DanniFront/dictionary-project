ec159f4cb8d51440d782e5b9ccc4297f922f31d5
DanniFront <mure.daniela@gmail.com>
2025-06-17 11:49:04 +0200

    Rendered the word meanings

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,8 +4,17 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
