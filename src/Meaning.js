1525c0e3707f87415f4ddeecfe0d48f44cd99515
DanniFront <mure.daniela@gmail.com>
2025-06-18 11:02:55 +0200

    Added new component and meaning Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>
+        <strong>Definition:</strong>
         {props.meaning.definition}
         <br />
+        <strong>Example:</strong>
         <em>{props.meaning.example}</em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
   );
