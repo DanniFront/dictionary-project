c0462748588424ca60e4d22a046a07f8bf2acbd1
DanniFront <mure.daniela@gmail.com>
2025-06-18 13:16:49 +0200

    Created a default keyword to the Dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,15 +3,14 @@ import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
-  function search(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    let apiKey = "748d5734be19te1af0e3a12aa9abaofd";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
   }
 
   function handleKeywordChange(event) {
@@ -19,21 +18,36 @@ export default function Dictionary() {
   }
 
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data);
   }
 
-  return (
-    <div className="Dictionary">
-      <h2>Search for a word in React Dictionary</h2>
-      <form onSubmit={search} className="Dictionary-form">
-        <input
-          type="search"
-          onChange={handleKeywordChange}
-          className="Dictionary-search"
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function search() {
+    let apiKey = "748d5734be19te1af0e3a12aa9abaofd";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <h2>Search for a word in React Dictionary</h2>
+        <form onSubmit={handleSubmit} className="Dictionary-form">
+          <input
+            type="search"
+            onChange={handleKeywordChange}
+            className="Dictionary-search"
+          />
+        </form>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
