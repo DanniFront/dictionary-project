998638eecb1402686b049f96723587596a3dbe1d
DanniFront <mure.daniela@gmail.com>
2025-06-19 14:56:38 +0200

    Integration API Photos

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
       <main>
-        <Dictionary defaultKeyword="clouds" />
+        <Dictionary defaultKeyword="cloud" />
       </main>
       <footer className="App-footer">
         Coded by{" "}
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import Results from "./Results";
 import axios from "axios";
+import Photos from "./Photos";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -17,14 +19,23 @@ export default function Dictionary(props) {
     setKeyword(event.target.value);
   }
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePhotoResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiKey = "748d5734be19te1af0e3a12aa9abaofd";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let photoApiKey = "748d5734be19te1af0e3a12aa9abaofd";
+    let photoApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photoApiKey}`;
+    axios.get(photoApiUrl).then(handlePhotoResponse);
   }
 
   function load() {
@@ -49,6 +60,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
